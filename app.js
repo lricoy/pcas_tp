a2ba4323871a9cc164e2335eeffdcb90d106dad1
Lucas Ricoy <ricoy.lucas@gmail.com>
2015-05-27 11:47:26 -0300

    Now printing data on a csv format

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var readValues = function() {
         var vi = prompt('Digite o valor de Vi para o dispositivo '+i + ': ');
         var si = prompt('Digite o valor de Si para o dispositivo '+i + ': ');
 
-        servers.push({ vi: parseInt(vi), si: parseInt(si) });
+        servers.push({ vi: parseFloat(vi), si: parseFloat(si) });
 
         callback(null, servers);
       }
@@ -37,45 +37,73 @@ var initValues = function(callback) {
 };
 
 var calcMVA = function(callback) {
+
+  printHeaders();
+
   for (var i = 1; i <= n; i++) {
 
-    results[i] = { t: [], s: 0, n: [] };
+    results[i] = { tempo_de_espera: [], s: 0, n: [], throughput: [], u:[] };
     var total_delay = 0;
 
     for (var j = 1; j <=m; j++) {
 
       // Calcula o tempo médio de espera, baseado no último loop
-      results[i].t[j] = (1.0 / servers[j-1].si) * (results[i-1].n[j] + 1.0);
+      results[i].tempo_de_espera[j] = (servers[j-1].si) * (results[i-1].n[j] + 1);
 
       // Calcula o throughput para a rede
-      total_delay += results[i].t[j];
+      total_delay += results[i].tempo_de_espera[j] * servers[j-1].vi;
     };
 
+    // Calcula o throughput do sistema e de cada dispositivo
     results[i].s = i / total_delay;
+
+    for (var j = 1; j <=m; j++) {
+      results[i].throughput[j] = results[i].s * servers[j-1].vi;
+    };
+
+    // Calcula a % de utilização de cada dispositivo e do sistema geral
+    for (var j = 1; j <=m; j++) {
+      results[i].u[j] = servers[j-1].si * results[i].throughput[j];
+    };
 
     // Calcula o número médio de clientes em cada fila
     for (var j = 1; j <=m; j++) {
-      results[i].n[j] = results[i].s * results[i].t[j];
+      results[i].n[j] = results[i].tempo_de_espera[j] * results[i].throughput[j];
     };
 
     // console.log(results);
 
-    printSystemData(i, results[i].s, total_delay);
+
+    // printSystemData(i, results[i].s, total_delay);
 
     for (var j = 1; j <=m; j++) {
-      printComponentData(j, results[i].n[j], results[i].t[j])
+      // printComponentData(j, results[i].n[j], results[i].tempo_de_espera[j], results[i].throughput[j], results[i].u[j]);
+        printCsvData(i, j, results[i].n[j], results[i].tempo_de_espera[j], results[i].throughput[j], results[i].u[j], results[i].s, total_delay);
     };
 
   };
 };
 
 var printSystemData = function(nCustomers, sysTruput, totalDelay) {
-  console.log("%d customers --- thruput = %s cust/sec and delay = %s sec \n",
-    nCustomers, sysTruput, totalDelay);
+  console.log("%d Clientes --- X0 = %s cliente/tempo and R = %s sec \n",
+    nCustomers, parseFloat(sysTruput).toFixed(3), parseFloat(totalDelay).toFixed(3));
 };
 
-var printComponentData = function(name, length, delay) {
-  console.log("    >>> n[%d] = %s --- t[%d] = %s \n", name, length, name, delay);
+var printHeaders = function() {
+  console.log("Num. Clientes\tM\tN()\tR\tX\tU\tX0\tR Sistema");
+};
+var printCsvData = function(i, j, n, r, x, u, x0, r_total) {
+  console.log("%s	%s	%s	%s	%s	%s	%s	%s	",
+    i, j, n, r, x, u, x0, r_total);
+};
+
+var printComponentData = function(name, length, delay, throughput, utilization) {
+  console.log("N[%d] = %s; R[%d] = %s; X[%d] = %s; U[%d] = %s; \n",
+    name, parseFloat(length).toFixed(3),
+    name, parseFloat(delay).toFixed(3),
+    name, parseFloat(throughput).toFixed(3),
+    name, parseFloat(utilization).toFixed(3)
+    );
 }
 
 
